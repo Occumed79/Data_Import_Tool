5e041a767decfadf023c8b36d5879a29aeba1f0c
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-20 03:19:27 -0700

    Refresh only due sources in scheduled runner

--- scripts/refresh_sources.py
+++ scripts/refresh_sources.py
@@ -6,5 +6,5 @@ from src.sources import refresh_all_active_sources
 
 
 if __name__ == "__main__":
-    results = refresh_all_active_sources()
+    results = refresh_all_active_sources(due_only=True)
     print(json.dumps(results, indent=2, default=str))
